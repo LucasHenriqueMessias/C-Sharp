58c4aaaa398f94689231f10fd48eee45a2b89ba9
LucasHenriquemessias <64761016+LucasHenriquemessias@users.noreply.github.com>
2021-04-26 20:05:28 -0300

    Add files via upload

---
+++ pTesteMetodos/pTesteMetodos/frmExercicio2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pTesteMetodos
+{
+    public partial class frmExercicio2 : Form
+    {
+        public frmExercicio2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            if (string.Compare(txtPalavra1.Text, txtPalavra2.Text, true) == 0)
+            {
+                MessageBox.Show("As palavras são iguais");
+            }
+            else
+            {
+                MessageBox.Show("As palavras são diferentes");
+            }
+        }
+
+        private void btnInserir1_Click(object sender, EventArgs e)
+        {
+            int metade = txtPalavra2.Text.Length / 2;
+
+            txtPalavra2.Text = txtPalavra2.Text.Substring(0, metade) + txtPalavra1.Text +
+                txtPalavra2.Text.Substring(metade, txtPalavra2.Text.Length - metade);
+        }
+    }
+}
